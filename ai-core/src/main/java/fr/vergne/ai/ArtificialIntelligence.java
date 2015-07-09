cfec369980e3fda65be1944708449a6fc49494c0
Matthieu Vergne <matthieu.vergne@gmail.com>
2015-07-09 05:21:40 +0200

    Revise method names to use more usual conventions.

--- ai-core/src/main/java/fr/vergne/ai/ArtificialIntelligence.java
+++ ai-core/src/main/java/fr/vergne/ai/ArtificialIntelligence.java
@@ -2,7 +2,7 @@ package fr.vergne.ai;
 
 public interface ArtificialIntelligence extends Runnable {
 
-	public <T> void registerSensor(Sensor<T> sensor);
+	public <Value> void addSensor(Sensor<Value> sensor);
 
-	public <T> void registerActuator(Actuator<T> actuator);
+	public void addActuator(Actuator actuator);
 }
